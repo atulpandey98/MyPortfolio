4a8f4cda9654d7c41ec728635b3e647925a61f0b
Atul Pandey <atulpan98@gmail.com>
2020-05-11 05:33:37 +0530

    initial commit

---
+++ scripts/main.js
@@ -0,0 +1,40 @@
+/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+} 
+
+const navSlide = () => {
+  const burger = document.querySelector('.burger');
+  const nav = document.querySelector('.nav-links');
+  const navLinks = document.querySelectorAll('.nav-links li');
+
+  //Toggle Nav
+  burger.addEventListener('click', () => {
+    nav.classList.toggle('nav-active');
+
+    //Animate Links
+    navLinks.forEach((link, index) => {
+      if(link.style.animation) {
+        link.style.animation = ``;
+      }
+      else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.8}s`;
+      }
+
+    });
+
+    //Burger Animation
+    burger.classList.toggle('toggle');
+  });
+}
+
+navSlide();
+
+
